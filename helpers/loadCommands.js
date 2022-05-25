cef26e33382895f33dbf4ed05e621d00083096f9
BrandonPessman <pessman.brandon@gmail.com>
2022-05-25 00:29:30 -0500

    add comments

--- helpers/loadCommands.js
+++ helpers/loadCommands.js
@@ -2,12 +2,22 @@ const fs = require('node:fs');
 
 module.exports = async (client) => {
     console.log("Loading up the commands...")
+
+    // Find each directory within commands
     fs.readdirSync('./commands/').forEach(dir => {
+        // Find each file (command), within the directory
         let dirCommands = fs.readdirSync(`./commands/${dir}`).filter(files => files.endsWith('.js'))
         for (const file of dirCommands) {
+            // Read in the command file
             const command = require(`../commands/${dir}/${file}`)
+
+            // Add the command to the collection
             client.commands.set(command.name.toLowerCase(), command.execute)
+
+            // Delete the import cache for optimization
             delete require.cache[require.resolve(`../commands/${dir}/${file}`)];
+
+            // Logging for devs
             console.log(`✔ '${dir}: ${command.name.toLowerCase()}'`)
         }
     })
--- helpers/loadEvents.js
+++ helpers/loadEvents.js
@@ -2,12 +2,22 @@ const fs = require('node:fs');
 
 module.exports = async (client) => {
     console.log("\nLoading up event listeners...")
+    
+    // Find each directory within events
     fs.readdirSync('./events/').forEach(dir => {
+        // Find each file (event), within the directory
         let dirEvents = fs.readdirSync(`./events/${dir}`).filter(files => files.endsWith('.js'))
         for (const file of dirEvents) {
+            // Read in the event file
             const event = require(`../events/${dir}/${file}`)
+
+            // Get the event name from filename
             const eventName = file.split('.')[0]
+
+            // Watch the event and give it it's function
             client.on(eventName, event)
+
+            // Logging for devs
             console.log(`✔ '${dir}: ${eventName}'`)
         }
     })
